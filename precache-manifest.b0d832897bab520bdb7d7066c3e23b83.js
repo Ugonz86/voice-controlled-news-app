45c636950c456e75189d9c1a2696085bd9922df4
Ugonz86 <ugonzalez86@gmail.com>
2023-05-10 16:56:12 -0700

    Updates

---
+++ precache-manifest.b0d832897bab520bdb7d7066c3e23b83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd9dea4eb2f4ced1771e",
+    "url": "/static/css/main.bc08ae15.chunk.css"
+  },
+  {
+    "revision": "cd9dea4eb2f4ced1771e",
+    "url": "/static/js/main.cd9dea4e.chunk.js"
+  },
+  {
+    "revision": "604244e176e83d3f00ab",
+    "url": "/static/js/1.604244e1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "089c829d955dafe11f802dbb30074551",
+    "url": "/static/media/alan.089c829d.jpg"
+  },
+  {
+    "revision": "a907e590b24592a98d0b745d2d0d17a7",
+    "url": "/index.html"
+  }
+];
